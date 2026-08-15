6e595a03947944420bd595eea08216d9d7cf40b3
hasxc765-lgtm <hasxc765@gmail.com>
2026-08-15 04:10:31 +0300

    Add new dose units for mL and mEq

--- js/data/common/doseUnits.js
+++ js/data/common/doseUnits.js
@@ -12,18 +12,26 @@ export const DOSE_UNITS = Object.freeze({
   MCG_PER_KG: "mcg/kg",
   MG_FIXED: "mg",
   MCG_FIXED: "mcg",
-  
+  ML_FIXED: "mL",
+  ML_PER_KG: "mL/kg",
+  MEQ_FIXED: "mEq",
+  MEQ_PER_KG: "mEq/kg",
+
   // Infusion Rate Units
   MCG_PER_KG_MIN: "mcg/kg/min",
   MCG_PER_KG_HOUR: "mcg/kg/hour",
   MG_PER_KG_HOUR: "mg/kg/hour",
   MCG_PER_MIN: "mcg/min",
+  MG_PER_MIN: "mg/min",
   MG_PER_HOUR: "mg/hour",
+  ML_PER_KG_MIN: "mL/kg/min",
 
-  // Volatiles & Concentrations
+  // Volatiles, Concentrations & Solutions
   PERCENT_END_TIDAL: "%_end_tidal",
   PERCENT_MAC: "%_mac",
   PERCENT_LIQUID: "percent_liquid",
   MG_PER_ML: "mg/mL",
-  MCG_PER_ML: "mcg/mL"
+  MCG_PER_ML: "mcg/mL",
+  MEQ_PER_ML: "mEq/mL",
+  MOLAR: "Molar"
 });
